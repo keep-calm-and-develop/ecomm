77ca32c76446413052343115fe5ebc0863a34f9f
Aniket <aniket@nirmata.com>
2023-06-16 11:03:53 +0530

    Sharing dependencies between apps

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
-console.log('container')
+console.log('container');
+
+productsMount(document.querySelector('#my-products'));
+
+cartMount(document.querySelector('#my-cart'));
